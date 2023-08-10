0e2586040bb9fdb0653f9ca935b24df66fd7e3c2
elafemad <elafemad02@gmail.com>
2023-08-10 09:23:59 -0700

    a C program that prints a sentance without using printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDOUT_FILENO, message, sizeof(message)-1);
+	char h[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDOUT_FILENO, h, sizeof(h) - 1);
 	return (1);
 }
