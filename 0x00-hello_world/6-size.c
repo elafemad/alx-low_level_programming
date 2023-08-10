1083014c6281f34ba7f007f77de75e9c9eb0da0c
elafemad <elafemad02@gmail.com>
2023-08-10 08:30:58 -0700

    A C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - print the size of multiple types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %c byte(s)\n", sizeof(char));
+	printf("Size of a int: %d byte(s)\n"), sizeof(int);
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %lld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %f byte(s)\n", size(float));
+	return (0);
+}
