313a45f0f702347dd83bac6fef53bf5d0e611b64
elafemad <elafemad02@gmail.com>
2023-08-11 01:02:25 -0700

    A  program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: use putchar to print  letters
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char elaf = 97;
+	
+	while(elaf < '{')
+	{
+	putchar(elaf);
+	elaf++;
+	}
+	putchar('\n');
+	return (0);
+}
+
