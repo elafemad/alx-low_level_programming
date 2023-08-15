cc1eb23dc6148f59c370995f3eb8c9012a14e387
elafemad <elafemad02@gmail.com>
2023-08-15 13:03:16 -0700

    a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add 2 num.
+ * @x: first number.
+ * @y: second num.
+ * Return: sum.
+ */
+int add(int x, int y)
+{
+	int sum = 0;
+
+	sum = x + y;
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * times_table - a function that prints the 9 times table, starting with 0.
+ *
+ * Return: empty.
+ */
+void times_table(void)
+{
+	int i,j,k,l,m;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j<= 9; j++)
+		{
+			k = i * y;
+}
