9168cb244317a6dd79219e1495f6cbbaff350a61
elafemad <elafemad02@gmail.com>
2023-08-15 10:25:46 -0700

    a function that prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,6 +3,7 @@
 /**
  * print_sign - check the code.
  * Discription:a function that prints the sign of a number.
+ * @n: is an input
  * Return: 0 if 0, 1 if + and -1 if -.
  */
 int print_sign(int n)
