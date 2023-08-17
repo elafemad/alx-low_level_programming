2aea692595b61a151448b9b32c6fba7ad0c70b17
elafemad <elafemad02@gmail.com>
2023-08-17 02:13:36 -0700

    fixing number 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_most_numbers: function that prints the numbers.
+ * print_most_numbers - function that prints the numbers.
  * Return: empty.
- * */
+ */
 void print_most_numbers(void)
 {
 	int i;
