f3c9dd8eae423136b2b107a254a6112ac275ea02
elafemad <elafemad02@gmail.com>
2023-08-18 11:30:58 -0700

    integer num

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ * print_number - a function that prints an integer.
+ * Discription : find intger
+ * @h: an intger.
+ * @m: an integer.
+ * Return : 0 .
+ */
+void print_number(int n)
+{
+	unsigned int h = m;
+
+	if (m < 0)
+	{
+		_putchar('-');
+		h = -h;
+	}
+	if ((h / 10) > 0)
+		print_number(h / 10);
+
+	_putchar((h % 10) + '0');
+}
