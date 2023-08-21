15fcda0136e5b2f60801129a5b5ec0e48ccb2c5b
elafemad <elafemad02@gmail.com>
2023-08-21 11:33:28 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - function print string in reverse.
+ * Description: function print string.
+ * @s: useless char.
+ * Return: void.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+
+	}
+	for (; i != '0'; --i)
+		_putchar(s[i]);
+	putchar('\n');
+}
