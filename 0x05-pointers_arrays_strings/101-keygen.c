0948b9b0966bd7c788e404df3cd4e9ea236eba89
elafemad <elafemad02@gmail.com>
2023-08-21 19:59:42 -0700

    a program that generates random valid passwords for the program

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - convert string to num.
+ *
+ * @s: string to be converted.
+ *
+ * Return: int value.
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
+}
