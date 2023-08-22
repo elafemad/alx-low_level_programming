3ed4074240b119b0d1e6ddbc081827f2ca83c63f
elafemad <elafemad02@gmail.com>
2023-08-21 19:55:23 -0700

    convert string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,8 +14,8 @@ int _atoi(char *s)
 	unsigned int num = 0;
 
 	do {
-		if (*s -- '-')
-			sign* = -1;
+		if (*s == '-')
+			sign *= -1;
 
 		else if (*s >= '0' && *s <= '9')
 			num = (num *10) + (*s - '0');
