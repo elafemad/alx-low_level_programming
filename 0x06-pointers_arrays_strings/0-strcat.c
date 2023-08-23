6b3110181f99ed09eef83064c47ace993e3d980f
elafemad <elafemad02@gmail.com>
2023-08-23 09:51:22 -0700

    cat 2 strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,17 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
+	int k;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
 	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[i + 1] = src[j];
+	}
+	for ( k = 0; k <= j; k++)
+	{
+		dest[i + k] = src[k];
 	}
 	return (dest);
 }
