78a18f1e047dadb9917d4f7adb34b9027a44d94a
elafemad <elafemad02@gmail.com>
2023-08-24 19:57:08 -0700

    fun print int

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *rot13(char *);
+void print_number(int n);
 int _strlen(char *s);
 int _putchar(char c);
 
