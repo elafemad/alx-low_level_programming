9ba6268f45b2930d0a7e966f6c68aef03ae0f4eb
elafemad <elafemad02@gmail.com>
2023-08-28 13:56:14 -0700

    diagonal square

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,14 +10,14 @@
 void print_diagsums(int *a, int size)
 {
 
-	int i, sum1, sum2;
+	int i, sum1 = 0, sum2 = 0;
 
 	for (i =  0; i < size; i++)
 	{
 		sum1 += *(a + i * size + i);
 		sum2 += *(a + i * size + (size - i - 1));
 		{
-			printf("Sum of diagonal elemnts: %d, %d\n", sum1, sum2);
+			printf("%d, %d\n", sum1, sum2);
 		}
 	}
 }
