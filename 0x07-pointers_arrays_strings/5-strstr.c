2f99d93f50ae5454ccdfd9060985277be3da4e08
elafemad <elafemad02@gmail.com>
2023-08-28 04:57:29 -0700

    function fill memory byte

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+
+
+
+#include "main.h"
+/**
+ * *_memset- function that fills memory with a constant byte.
+ * @s:pointer
+ * @b:const byte
+ * @n:an integer
+ * Return: pointer.
+ */
+ char *_memset(char *s, char b, unsigned int n)
+ {
+
+ }
