4963205b6954369fe71f1e02dbb80509359b211f
elafemad <elafemad02@gmail.com>
2023-08-28 14:09:23 -0700

    advance

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,21 +1,13 @@
 
 #include "main.h"
-#include <math.h>
 /**
- * print_diagsums- function print sum of diagonals of a square.
+ * set_string - sets the value to pointer
  * Description: diagonals od square.
- * *s:pointer of array
- * to: input
+ * @s:pointer of array
+ * @to: input
  * Return: void no return,
  */
 void set_string(char **s, char *to)
 {
-
-	int i, sum1, sum2;
-
-	for (i =  0; i < size; i++)
-	{
-		sum1 += *(a + i * size + i);
-		sum2 += *(a + i * size + (size - i - 1));
-	}
+	*s = to;
 }
