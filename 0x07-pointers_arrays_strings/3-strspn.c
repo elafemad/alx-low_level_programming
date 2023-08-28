44ded9b718740aa31c2c60f87280118188e910d7
elafemad <elafemad02@gmail.com>
2023-08-28 08:46:04 -0700

    function locate a char

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,14 +1,27 @@
-
-
 #include "main.h"
 /**
- * *_memset- function that fills memory with a constant byte.
- * @s:pointer
- * @b:const byte
- * @n:an integer
- * Return: pointer.
+ * _strspn- function that fills memory with a constant b
+ * @s:string to be searched
+ * @accept:string contain char to match
+ * Return: num.
  */
- char *_memset(char *s, char b, unsigned int n)
- {
+unsigned int _strspn(char *s, char *accept)
+{
+	int j, k;
 
- }
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		for (k = 0; accept[k] != '\0'; k++)
+		{
+			if (s[j] == accept[k])
+			{
+				break;
+			}
+		}
+	}
+	if (accept[k] == '\0')
+	{
+		return (j);
+	}
+	return (j);
+}
