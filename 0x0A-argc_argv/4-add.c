e9db0da1ce261d94cbbe3196680a4eaab7386ee3
elafemad <elafemad02@gmail.com>
2023-09-01 12:39:21 -0700

    add 2 positive num

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 /**
- * main - entry point
- * @argc: number of argument
- * @argv: array
- * Return: o if no num is passed, error is symbol is passed elase return the sum
+ *main - adds positive numbers
+ *@argc: number of arguments
+ *@argv: array of arguments
+ *Return: 0 on success, 1 on failure
  */
 int main(int argc, char *argv[])
 {
-	int i, j, sum;
-	sum = 0;
-
-	for (i = 0; i < argc; i++)
+	int i, j, sum = 0;
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		if ()
-		{
-			return (0);
-		}
+		sum += atoi(argv[i]);
 	}
-	sum += atoi(argv[i]);
 	printf("%d\n", sum);
-
 	return (0);
 }
