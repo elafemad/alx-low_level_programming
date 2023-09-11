9ce3b2b2e7379307bec6250ae4c338b667b8c492
elafemad <elafemad02@gmail.com>
2023-09-11 10:35:07 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include<stdio.h>
+/**
+ * struct dog - new struct
+ * @name: char
+ * @age: integer
+ * @owner: char
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
