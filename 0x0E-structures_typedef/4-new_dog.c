0c48544e9c714435b766b738dbe07f3828994342
elafemad <elafemad02@gmail.com>
2023-09-11 12:07:09 -0700

    A function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,5 +18,57 @@ int _strlen(char *str)
 /**
  * _strcopy - copy string
  * @dest: pointer char.
- * @src
+ * @src: ponter to char.
+ * Return: returns a type of char.
+ */
 
+char *_strcopy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] ; i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog.
+ * @name: The name.
+ * @age: The age.
+ * @owner: The owner.
+ * Return: pointer.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (new->name == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (new->owner == NULL)
+	{
+		free(new->name);
+		free(new);
+		return (NULL);
+	}
+
+	new->name = _strcopy(new->name, name);
+	new->age = age;
+	new->owner = _strcopy(new->owner, owner);
+
+	return (new);
+}
