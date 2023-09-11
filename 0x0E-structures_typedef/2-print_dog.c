93e16588153491ec1f683e57c79e355f488c945c
elafemad <elafemad02@gmail.com>
2023-09-11 11:44:34 -0700

    print DOG

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,15 +10,31 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	else if (d->name == NULL)
+
+	if (d->name == NULL)
 	{
-		printf("j
-	}
-	else if ()
-	{
-	}
-	else if ()
-	{
+		printf("nil\n");
 	}
 	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+
+	if (d->age < 0)
+	{
+		printf("nil\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+
+	if (d->owner == NULL)
+	{
+		printf("nil\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 }
