8b51b7c3a8aaa4968ff271764c349dbe9d3ed4be
elafemad <elafemad02@gmail.com>
2023-09-11 11:23:28 -0700

    New type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,13 +16,12 @@ struct dog
 	char *owner;
 
 };
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
 
-} dog_t;
+/**
+ * dog_t - Typedef for struct dog.
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
