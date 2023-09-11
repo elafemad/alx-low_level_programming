ee9b663369d08871b181c1da74484861b176c1f1
elafemad <elafemad02@gmail.com>
2023-09-11 10:42:43 -0700

    fun initialize

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - function initalize a var of struct dog.
+ * @d: pointer.
+ * @name: char.
+ * @age: integer.
+ * @owner: char.
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
