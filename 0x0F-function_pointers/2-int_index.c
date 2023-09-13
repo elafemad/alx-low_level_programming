8e25e3dd6bd7fe4512f5aae048819c439d4dbf16
elafemad <elafemad02@gmail.com>
2023-09-13 04:05:35 -0700

    function that searches for an integer.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,13 +1,24 @@
-
 #include "function_pointers.h"
 /**
- * array_iterator - function that execute a fun as a parameter on 
- * @array: input
+ * int_index - function that searches for an integer.
+ * @array: pointer to array
  * @size: size of array
- * @action:a pointer to function
- * Return: none
+ * @cmp:a pointer to function
+ * Return: 0
  */
-void array_iterator(int *array, size_t size, void (*action)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
 
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+	}
+
+	return (-1);
 }
