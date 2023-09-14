a48f13183e9170141d6640cef895536b4b8e13d0
elafemad <elafemad02@gmail.com>
2023-09-14 08:55:43 -0700

    function print string

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,11 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		num = va_arg(args, unsigned int);
 		printf("%d", num);
-		if (separator == NULL)
-		{
-			return;
-		}
-		if (i < n - 1)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 
 	}
