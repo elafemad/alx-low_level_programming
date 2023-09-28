bf501c714279cc20e423db7243d198526066e4a9
elafemad <elafemad02@gmail.com>
2023-09-28 12:59:02 -0700

    fun set value of bit to 0 at given index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,19 @@
-
 #include "main.h"
 /**
- * binarry_to_unit - func convert binary to unsigned int
- * @b: pointer
- * Return: Always 0.
+ * clear_bit - function that sets the value of a bit to 0 at a given index.
+ * @n: pointer
+ * @index: index
+ * Return: 1.
  */
-unsigned int binary_to_uint(const char *b)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int i = 1;
 
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+
+	i = ~(i << index);
+	*n = *n & i;
+
+	return (1);
 }
