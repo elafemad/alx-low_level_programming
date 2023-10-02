d9aa218ecd30a39c35569b9364100db964f187aa
elafemad <elafemad02@gmail.com>
2023-10-02 02:49:12 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * create_file -  a function that creates a file.
- *@filename: pointer.
- @text_content : pointer to array
- *Return 1 on success -1 in failaure.
+ * create_file - a function that creates a file
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ * Return: 1 on success -1 in failaure.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, fr;
 
 
 	if (filename == NULL)
@@ -20,8 +20,12 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-		write(fd, text_content, strlen(text_content));
-		close(fd);
+		fr = write(fd, text_content, strlen(text_content));
 	}
+	if (fd == -1 || fr == -1)
+	{
+		return (-1);
+	}
+	close(fd);
 	return (1);
 }
