bfe5ddb5b2ff9ff5bf9e28230d0d9ff456de1560
elafemad <elafemad02@gmail.com>
2023-10-02 16:06:09 +0300

    fix 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,56 +5,87 @@
 #include <string.h>
 
 /**
- * main - function that copy.
- * @ac: num of argument.
+ * test - function to validate arguments.
+ * @ac: number of arguments.
  * @av: arguments.
- * Return: 1 on success.
+ * Return: 0 on success, 1 on failure.
+ */
+int test(int ac, char **av)
+{
+    if (ac != 3)
+    {
+        dprintf(STDOUT_FILENO, "Usage: %s file_from file_to\n", av[0]);
+        return 1;
+    }
+    
+    if (av[1] == NULL)
+    {
+        dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
+        return 1;
+    }
+    
+    return 0;
+}
+
+/**
+ * main - function that copies a file.
+ * @ac: number of arguments.
+ * @av: arguments.
+ * Return: 0 on success, 1 on failure.
  */
 int main(int ac, char **av)
 {
-	int f1, f2, r1, w2;
-	int result1 = close(fd);
-	char b[1024];
+    int f1, f2, r1, w2;
+    char b[1024];
+    
+    if (test(ac, av) != 0)
+    {
+        exit(97);
+    }
 
-	if (ac > 3 && ac < 2)
-	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	if (av[1] == NULL)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	else
-	{
-		f1 = open(av[1], O_RDWR);
-		r1 = read(f1, b, 1024);
-		if (r1 == -1)
-		{
-			dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
-	}
-	f2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (f2 == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[2]);
-		exit(99);
-	}
-	w2 = write(f2, b, strlen(b));
-	if (w2 == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[2]);
-		exit(99);
-	}
-	
-	if (result1 == -1) {
-        dprintf(stderr, "Error: Can't close fd %d\n", fd);
+    f1 = open(av[1], O_RDONLY);
+    if (f1 == -1)
+    {
+        dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
+        exit(98);
+    }
+
+    r1 = read(f1, b, sizeof(b));
+    if (r1 == -1)
+    {
+        dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
+        close(f1);
+        exit(98);
+    }
+
+    f2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (f2 == -1)
+    {
+        dprintf(STDOUT_FILENO, "Error: Can't write to file %s\n", av[2]);
+        close(f1);
+        exit(99);
+    }
+
+    w2 = write(f2, b, r1);
+    if (w2 == -1)
+    {
+        dprintf(STDOUT_FILENO, "Error: Can't write to file %s\n", av[2]);
+        close(f1);
+        close(f2);
+        exit(99);
+    }
+
+    if (close(f1) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
         perror("close");
         exit(100);
     }
-	close(f1);
-	close(f2);
-	return (1);
+    
+    if (close(f2) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2);
+        perror("close");
+        exit(100);
+    }
+
+    return 0;
 }
