25221c6e8c3db33c07447f7c01d9b40f513008f3
elafemad <elafemad02@gmai.com>
2023-11-27 01:30:10 -0800

    fix task 3cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,19 +15,19 @@ int copyFile(const char *source, const char *destination);
 
 int test(int ac, char **av)
 {
-        if (ac != 3)
-        {
-                dprintf(STDOUT_FILENO, "Usage: %s file_from file_to\n", av[0]);
-                return (1);
-        }
+	if (ac != 3)
+	{
+		dprintf(STDOUT_FILENO, "Usage: %s file_from file_to\n", av[0]);
+		return (1);
+	}
 
-        if (av[1] == NULL)
-        {
-                dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
-                return (1);
-        }
+	if (av[1] == NULL)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
+		return (1);
+	}
 
-        return (0);
+	return (0);
 }
 
 /**
@@ -39,49 +39,49 @@ int test(int ac, char **av)
 
 int copyFile(const char *source, const char *destination)
 {
-        int f1, f2, r1, w2;
-        char buffer[BUFFER_SIZE];
+	int f1, f2, r1, w2;
+	char buffer[BUFFER_SIZE];
 
-        f1 = open(source, O_RDONLY);
-        if (f1 == -1)
-        {
-                dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", source);
-                return (1);
-        }
+	f1 = open(source, O_RDONLY);
+	if (f1 == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", source);
+		return (1);
+	}
 
-        r1 = read(f1, buffer, sizeof(buffer));
-        if (r1 == -1)
-        {
-                dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", source);
-                close(f1);
-                return (1);
-        }
+	r1 = read(f1, buffer, sizeof(buffer));
+	if (r1 == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", source);
+		close(f1);
+		return (1);
+	}
 
-        f2 = open(destination, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-        if (f2 == -1)
-        {
-                dprintf(STDOUT_FILENO, "Error: Can't write to file %s\n", destination);
-                close(f1);
-                return (1);
-        }
+	f2 = open(destination, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (f2 == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't write to file %s\n", destination);
+		close(f1);
+		return (1);
+	}
 
-        w2 = write(f2, buffer, r1);
-        if (w2 == -1)
-        {
-                dprintf(STDOUT_FILENO, "Error: Can't write to file %s\n", destination);
-                close(f1);
-                close(f2);
-                return (1);
-        }
+	w2 = write(f2, buffer, r1);
+	if (w2 == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't write to file %s\n", destination);
+		close(f1);
+		close(f2);
+		return (1);
+	}
 
-        if (close(f1) == -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
-                perror("close");
-                return (1);
-        }
+	if (close(f1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
+		perror("close");
+		return (1);
+	}
 
-        return (0);
+	return (0);
 }
 
 
@@ -93,15 +93,15 @@ int copyFile(const char *source, const char *destination)
  */
 int main(int ac, char **av)
 {
-        if (test(ac, av) != 0)
-        {
-                exit(1);
-        }
+	if (test(ac, av) != 0)
+	{
+		exit(1);
+	}
 
-        if (copyFile(av[1], av[2]) != 0)
-        {
-                exit(1);
-        }
+	if (copyFile(av[1], av[2]) != 0)
+	{
+		exit(1);
+	}
 
-        return (0);
+	return (0);
 }
