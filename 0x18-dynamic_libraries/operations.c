25207789af697de0773ba6a57e5aa51be9694104
elafemad <elafemad02@gmai.com>
2024-01-17 18:11:38 -0800

    task 1

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - addition operation
+ * @a: number one
+ * @b: number two
+ * Return: the + of the numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - Subtraction operation
+ * @a: number one
+ * @b: number two
+ * Return: the - of the numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplication operation
+ * @a: number one
+ * @b: number two
+ * Return: the * of the numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - division operation
+ * @a: number one
+ * @b: number two
+ * Return: the / of the numbers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - %
+ * @a: number one
+ * @b: number two
+ * Return: the % of the numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
